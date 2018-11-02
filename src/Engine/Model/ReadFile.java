db097f84a0e1068d9dcfbcb36abf94d3e24314ea
nadavbar <nadav.bar-david@intel.com>
2018-11-02 12:42:08 +0200

    ReadFile Works. The readAllFiles method split all documents from each file in curpos. Each document is created with his DOCNO as the file name. Whole process takes around 40 minutes on my laptop.
    One thing to do in the future:
    After exporting the documents from the file, I think we should delete the main file.

--- src/Engine/Model/ReadFile.java
+++ src/Engine/Model/ReadFile.java
@@ -1,33 +1,40 @@
 package Engine.Model;
 
-import sun.misc.IOUtils;
-
 import java.io.*;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toSet;
 
 
 public class ReadFile {
-    /* This map will contains the <DOCNO> as a key and the full document string as value*/
-    public HashMap<String, String> documents;
+    /* This map will contains the <DOCNO> as a key and the file path as value*/
+    public ArrayList<String> filesPathsList;
 
-    public void readAllFiles() {
-        final File folder = new File("d:\\documents\\users\\bardanad\\Documents\\Engine\\corpus");
-        ArrayList<String> filesPathsList = listFilesForFolder(folder);
-        getDocuments(filesPathsList);
+    public ReadFile(){
+        filesPathsList = new ArrayList<>();
     }
 
-    private void getDocuments(ArrayList<String> filesPathsList) {
+    public void readAllFiles() {
+        final File folder = new File("C:\\Users\\nadavbar\\Documents\\SearchEngineCorpus\\corpusTest");
+        listFilesOfFolder(folder);
+        Long startTime = System.currentTimeMillis();
+        System.out.println("Starting to split documents at: " + startTime);
+        generateDocuments();
+        Long finishTime = System.currentTimeMillis();
+        System.out.println("Finish to split documents at: " + finishTime);
+        Long totalTime = (startTime - finishTime);
+        System.out.println("Total time: " + totalTime);
+
+    }
+
+    private void generateDocuments() {
         for (int i = 0; i < filesPathsList.size(); i++) {
+            String documentFilePath = filesPathsList.get(i);
             BufferedReader br = null;
             try {
                 br = new BufferedReader(new FileReader(filesPathsList.get(i)));
@@ -43,6 +50,9 @@ public class ReadFile {
                     sb.append("\n");
                     line = br.readLine();
                 }
+                String parentFilePath = documentFilePath;
+                String parentDirPath = documentFilePath.substring(0, documentFilePath.lastIndexOf('\\'));
+                splitDocumentsFromFile(sb.toString(), parentDirPath);
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -55,25 +65,51 @@ public class ReadFile {
         }
     }
 
-    public ArrayList<String> listFilesForFolder(final File folder) {
-        ArrayList<String> filesPaths = new ArrayList<>();
-        for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
-            } else {
-                filesPaths.add(fileEntry.getPath());
+    private void splitDocumentsFromFile(String fileContent, String path) {
+        String[] fileDocuments = fileContent.split("</DOC>");
+        for (int i = 0; i < fileDocuments.length; i++) {
+            String currentFullDocument = fileDocuments[i];
+            String docNumber = getDocNumber(currentFullDocument);
+            if (docNumber != null){
+                File document = new File(path + File.separator + docNumber);
+                try {
+                    document.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(document));
+                    writer.write(currentFullDocument);
+                    writer.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        return filesPaths;
     }
 
-
-    private ArrayList<String> getFilesPathList(Stream<Path> paths) {
-        paths.forEach(System.out::println);
+    private String getDocNumber(String fileDocument) {
+        String[] str = fileDocument.split("DOCNO>");
+        if (str.length > 1) {// && str[1].split(" ").length > 1) {
+            if (str[1].contains(" "))
+                str[1].replaceAll("\\s+","");
+            int indexOfArrow = str[1].indexOf("<");
+            String docNumber = str[1].substring(0, indexOfArrow);
+            return docNumber;
+        }
         return null;
     }
+
+    public void listFilesOfFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesOfFolder(fileEntry);
+            } else {
+                filesPathsList.add(fileEntry.getPath());
+            }
+        }
+    }
 }
-//    public void readAllFiles(){
-//        try (Stream<Path> paths = Files.walk(Paths.get("/home/you/Desktop"))) {
-//            paths.filter(Files::isRegularFile).forEach(System.out::println);
 
