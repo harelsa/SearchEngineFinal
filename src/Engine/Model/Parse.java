b2e3153f361613f578f94989cdb83f43ef509b69
Harel <harels55@gmail.com>
2018-11-03 12:44:28 +0200

    added parser open source stanford & maven

--- src/Engine/Model/Parse.java
+++ src/Engine/Model/Parse.java
@@ -1,5 +1,8 @@
 package Engine.Model ;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.stanford.nlp.trees.Tree;
@@ -22,16 +25,59 @@ public class Parse {
     private final LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
 
     public Tree parse(String str) {
+        str = remove_stop_words(str) ;
         List<CoreLabel> tokens = tokenize(str);
         Tree tree = parser.apply(tokens);
         return tree;
     }
+
+
 
     private List<CoreLabel> tokenize(String str) {
         Tokenizer<CoreLabel> tokenizer =
                 tokenizerFactory.getTokenizer(
                         new StringReader(str));
         return tokenizer.tokenize();
+    }
+
+    private String remove_stop_words(String str) {
+        String res = "" ;
+        int k = 0, i, j;
+        ArrayList<String> wordsList = new ArrayList<String>();
+        ArrayList<String> result = new ArrayList<String>();
+        String sCurrentLine;
+        String[] stopwords = new String[2000];
+        try {
+            FileReader fr = new FileReader("src\\Engine\\resources\\stop_words.txt"); // read stop words from the file
+            BufferedReader br = new BufferedReader(fr);
+            while ((sCurrentLine = br.readLine()) != null) {
+                stopwords[k] = sCurrentLine;
+                k++;
+            }
+
+            StringBuilder builder = new StringBuilder(str);
+            String[] words = builder.toString().split("\\s");
+            for (String word : words) {
+                wordsList.add(word);
+                result.add(word) ;
+            }
+            for (int ii = 0; ii < wordsList.size(); ii++) {
+                for (int jj = 0; jj < k; jj++) {
+                    if (stopwords[jj].contains(wordsList.get(ii).toLowerCase())) {
+                        result.remove(wordsList.get(ii));
+                        break;
+                    }
+                }
+
+            }
+            for (String s : result) {
+                 res +=  s + " ";
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        return res ;
     }
 
 }
