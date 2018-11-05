d4935a64655005c937896bf8516ed86d63be1d05
harelsa <harels55@gmail.com>
2018-11-05 16:43:32 +0200

    delete stanford

--- src/Engine/Model/Document.java
+++ src/Engine/Model/Document.java
@@ -5,6 +5,11 @@ import java.util.TreeSet;
 public class Document {
     private String docNo;
     private String parentFilePath;
+    private int max_tf  = 0 ; // frequancy of the most common term in doc
+    private int unique_t  = 0 ; // quantity of unuque terms in doc
+    private String city = "" ; // city of doc - appear in <F P=104> ...</F>
+
+
 
     public Document(String docNo, String parentFilePath) {
         this.docNo = docNo;
---
+++ src/Engine/Model/Term.java
@@ -0,0 +1,14 @@
+package Engine.Model;
+
+import java.util.HashMap;
+
+public class Term {
+    private int df = 0 ; // the number of docs the term is appearing
+    private int tf = 0 ; // the number of
+    private HashMap< Document , Integer > docs  ;// < doc obj , tr - number tomes the term appear in the doc >
+
+
+
+
+
+}
