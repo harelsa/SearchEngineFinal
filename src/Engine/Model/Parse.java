0d4a1b2b4b6bb47e62ff346add436f80799bc6e3
bardanad <nadavbardavid@gmail.com>
2018-11-06 18:44:17 +0200

    1/2 Date format added

--- src/Engine/Model/Parse.java
+++ src/Engine/Model/Parse.java
@@ -1,8 +1,6 @@
 package Engine.Model ;
-import com.sun.deploy.util.StringUtils;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.StringReader;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +13,9 @@ public class Parse {
 
     //Pattern NumberThousand = Pattern.compile("\\d* \\w Thousand");
     Pattern NumberSize = Pattern.compile("\\d*" + " " + "(Thousand|Million|Billion|percent|percentage|Dollars)");
+    Pattern DATE_DD_MONTH = Pattern.compile("\\d*" + " " + "(January|February|March|April|May|June|July|August|September|October|November|December)");
+    //"|JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGOST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)")
+    Pattern DATE_MONTH_DD = Pattern.compile("\\d*" + " " + "(Thousand|Million|Billion|percent|percentage|Dollars)");
 
 
     public HashSet<String> parse(String text) {
@@ -32,52 +33,120 @@ public class Parse {
         }
     }
 
-    private void checkValidTerm(String word) {
-        String term = ""; // the finel term !
-
-        word = word.replaceAll("[]\\[()?\",]", ""); // clean token
-        if ( ! Character.isDigit(word.charAt(0)) )  word = word.replaceAll("[.]", ""); // clean token
+    private void checkValidTerm(String token) {
+        String term = null; // the finel term !
+        token = token.replaceAll("[]\\[()?\",]", ""); // clean token
+        if ( ! Character.isDigit(token.charAt(0)) )  token = token.replaceAll("[.]", ""); // clean token
 
         String anotherToken = stringTokenizer.nextToken(); // save next token to check
+        /* Cleaning anotherToken */
+        anotherToken = anotherToken.replaceAll("[]\\[()?\",]", "");
+        if ( ! Character.isDigit(anotherToken.charAt(0)) )
+            anotherToken = anotherToken.replaceAll("[.]", "");
 
-        anotherToken = anotherToken.replaceAll("[]\\[()?\",]", ""); // clean token
-        if ( ! Character.isDigit(anotherToken.charAt(0)) )  anotherToken = anotherToken.replaceAll("[.]", "");
-
-        String newWord = word + " " + anotherToken;
-
-        // check <decimal + NumberSize >
-        System.out.println("Pattern print: " + NumberSize.pattern());
-        Matcher m = NumberSize.matcher(word + " " + anotherToken);
-        if(m.find()) {
-            String temp =anotherToken ;
-            switch (temp ){
-                case "Thousand" :
-                    term = word + "K" ;
-                    break;
-                case "Million" :
-                    term = word + "M" ;
-                    break;
-                case "Billion" :
-                    term = word + "B" ;
-                    break;
-                case "percent" :
-                    term = word + "%" ;
-                    break;
-                case "percentage" :
-                    term = word + "%" ;
-                    break;
-                case "Dollars" :
-                    term = word + "%" ;
-                    break;
-            }
-
+        term = getSpecialTermFromTwoTokens(token, anotherToken);
+        if (term != null){
+            documentTerms.add(term);
+            System.out.println("Term added: " + term);
+            return;
         }
-        System.out.println( term );
+        else{
 
-        //documentTerms.add(term);
+            if (stringTokenizer.countTokens() > 0) // If "anotherToken" is not the last token.
+                checkValidTerm(anotherToken);
+            else{
+                documentTerms.add(anotherToken);
+                System.out.println("Term added: " + term);
+            }
+        }
     }
 
+    private String getSpecialTermFromTwoTokens(String token, String anotherToken) {
+        // check <decimal + NumberSize >
+        Matcher numberSizeMatcher = NumberSize.matcher(token + " " + anotherToken);
+        if(numberSizeMatcher.find())
+            return PairTokensIsNumberFormat(token, anotherToken);
+        Matcher dateFormatMatcher = DATE_DD_MONTH.matcher(token + " " + anotherToken);
+        if (dateFormatMatcher.find())
+            return PairTokensIsDateFormat(token, anotherToken);
+        Matcher dateFormatMatcher2 = DATE_MONTH_DD.matcher(token + " " + anotherToken);
+        if (dateFormatMatcher2.find())
+            return PairTokensIsDate2Format(token, anotherToken);
+        return null;
+    }
 
+    private String PairTokensIsNumberFormat(String token, String anotherToken) {
+        String term = "";
+        String temp =anotherToken ;
+        switch (temp ){
+            case "Thousand" :
+                term = token + "K" ;
+                break;
+            case "Million" :
+                term = token + "M" ;
+                break;
+            case "Billion" :
+                term = token + "B" ;
+                break;
+            case "percent" :
+                term = token + "%" ;
+                break;
+            case "percentage" :
+                term = token + "%" ;
+                break;
+            case "Dollars" :
+                term = token + "$" ;
+                break;
+        }
+        return term;
+    }
+    private String PairTokensIsDate2Format(String token, String anotherToken) {
+        return null;
+    }
+
+    private String PairTokensIsDateFormat(String token, String anotherToken) {
+        String term = "";
+        String temp =anotherToken ;
+        switch (temp ){
+            case "January" :
+                term = "01-" + token ;
+                break;
+            case "February" :
+                term = "02-" + token ;
+                break;
+            case "March" :
+                term = "03-" + token ;
+                break;
+            case "April" :
+                term = "04-" + token ;
+                break;
+            case "May" :
+                term = "05-" + token ;
+                break;
+            case "June" :
+                term = "06-" + token ;
+                break;
+            case "July" :
+                term = "07-" + token ;
+                break;
+            case "Augost" :
+                term = "08-" + token ;
+                break;
+            case "September" :
+                term = "09-" + token ;
+                break;
+            case "October" :
+                term = "10-" + token ;
+                break;
+            case "November" :
+                term = "11-" + token ;
+                break;
+            case "December" :
+                term = "12-" + token ;
+                break;
+        }
+        return term;
+    }
 
 
     public String remove_stop_words(String str) {
