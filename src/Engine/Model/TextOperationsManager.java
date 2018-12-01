bc2ab1898a9ff66923511647c692937084cc0bb0
bdnadav <38585321+bdnadav@users.noreply.github.com>
2018-12-01 19:05:24 +0200

    Merge branch 'master' of https://github.com/bdnadav/SearchEngine
    # Conflicts:
    #	src/Engine/Model/TextOperationsManager.java

--- src/Engine/Model/TextOperationsManager.java
+++ src/Engine/Model/TextOperationsManager.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.Array;
